13d96cc2036969661ae68254c734314b634882c7
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-05-16 19:27:03 +0530

    packages: add unlighthouse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 
 export default defineNuxtConfig({
+  unlighthouse: {
+    scanner: {
+      // simulate a desktop device
+      device: 'desktop'
+    }
+  },
   app: {
     layoutTransition: { name: 'layout', mode: 'out-in' },
     head: {
@@ -9,26 +15,34 @@ export default defineNuxtConfig({
       // ],
       script: [
         // Insert your Google Tag Manager Script here
-        { src: 'https://js.stripe.com/v3/', async: true, type: 'text/partytown' },
-        { src: 'https://connect.facebook.net/en_US/sdk.js', async: true, type: 'text/partytown' }
+        {
+          src: 'https://js.stripe.com/v3/',
+          async: true,
+          type: 'text/partytown'
+        },
+        {
+          src: 'https://connect.facebook.net/en_US/sdk.js',
+          async: true,
+          type: 'text/partytown'
+        }
       ]
     }
   },
   css: ['/assets/main.css'],
   imports: {
-    dirs: ['stores'],
+    dirs: ['stores']
   },
   typescript: {
     shim: false,
     tsConfig: {
       exclude: ['node_modules', 'dist'],
       compilerOptions: {
-        strict: true,
-      },
-    },
+        strict: true
+      }
+    }
   },
   nitro: {
-    preset: 'render-com',
+    preset: 'render-com'
   },
   modules: [
     '@nuxtjs/tailwindcss',
@@ -39,16 +53,17 @@ export default defineNuxtConfig({
     '@nuxtjs/supabase',
     '@nuxtjs/color-mode',
     '@nuxt/image-edge',
+    '@unlighthouse/nuxt',
     [
       '@pinia/nuxt',
       {
-        autoImports: ['defineStore', 'acceptHMRUpdate'],
-      },
-    ],
+        autoImports: ['defineStore', 'acceptHMRUpdate']
+      }
+    ]
   ],
   partytown: {
     // For google analytics
-    forward: ['dataLayer.push'],
+    forward: ['dataLayer.push']
   },
   runtimeConfig: {
     // The private keys which are only available within server-side
@@ -61,7 +76,7 @@ export default defineNuxtConfig({
   supabase: {
     // Options: https://supabase.nuxtjs.org/get-started#options
     url: process.env.SUPABASE_URL,
-    key: process.env.SUPABASE_KEY,
+    key: process.env.SUPABASE_KEY
   },
   build: {
     transpile: ['']
