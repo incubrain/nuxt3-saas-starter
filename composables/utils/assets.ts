4c77537be41f5a5dabc9d9d0f60d3616c83182a3
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-05-20 17:45:13 +0530

    packages: nuxi upgrade to 3.5
    nuxt-icon
    eslint
    unlighthouse

--- composables/utils/assets.ts
+++ composables/utils/assets.ts
@@ -1,3 +1,4 @@
+// this is a helper function to dynamically display images from the assets folder. required as of 18 May 2023
 export function dynamicAsset(path: string): string {
   const assets = import.meta.glob('~/assets/**/*', {
     eager: true,
