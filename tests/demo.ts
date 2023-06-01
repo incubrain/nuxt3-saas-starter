9026a00f9cb3e12b3d12ff533ee5e300e22774be
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-06-01 11:20:18 +0530

    packages: testing - @nuxt/test-utils and vitest

---
+++ tests/demo.ts
@@ -0,0 +1,12 @@
+import { describe, test } from 'vitest'
+import { setup, $fetch } from '@nuxt/test-utils'
+
+describe('My test', async () => {
+  await setup({
+    // test context options
+  })
+
+  test('my test', () => {
+    // ...
+  })
+})
