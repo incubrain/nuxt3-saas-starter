bf5ed40e4b97b8fecceacd1785727f77e1cc2ef0
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-06-01 11:43:16 +0530

    fix: scrollToTop using AppBody

--- app/router.options.js
+++
@@ -1,6 +0,0 @@
-// This is a workaround for the issue with the scrollBehavior still present in Nuxt 3.0
-export default {
-  scrollBehavior () {
-    return false
-  }
-}
---
+++ components/AppBody.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    ref="scrollContainer"
+    class="w-full h-[calc(100vh-50px)] lg:h-[calc(100vh-60px)] relative overflow-y-scroll"
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+const scrollContainer = ref(null)
+
+onMounted(() => {
+  watch(route, () => {
+    if (scrollContainer.value) {
+      scrollContainer.value.scrollTop = 0
+    }
+  })
+})
+</script>
+
+<style scoped></style>
