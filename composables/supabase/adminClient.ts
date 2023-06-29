4c4a4f03b3e43ee3c8ed8f5392db94107fefc9a9
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-06-29 21:43:06 +0530

    fix(supabase): No storage opt exists presist err

--- composables/supabase/adminClient.ts
+++ composables/supabase/adminClient.ts
@@ -4,7 +4,16 @@ const adminClient = () => {
   const cfg = useRuntimeConfig()
   const serviceKey = cfg.SUPABASE_SERVICE_KEY
   const url = 'http://localhost:3000/'
-  return createClient(url, serviceKey)
+  return createClient(url, serviceKey, {
+    db: {
+      schema: 'public'
+    },
+    auth: {
+      autoRefreshToken: true,
+      persistSession: false,
+      detectSessionInUrl: true
+    }
+  })
 }
 
 export default adminClient
--- server/utils/supabaseClient.ts
+++ server/utils/supabaseClient.ts
@@ -5,5 +5,14 @@ export default function() {
   const config = useRuntimeConfig()
   const supaUrl = config.public.SUPABASE_URL
   const supaKey = config.public.SUPABASE_KEY
-  return createClient<Database>(supaUrl, supaKey)
+  return createClient<Database>(supaUrl, supaKey, {
+    db: {
+      schema: 'public'
+    },
+    auth: {
+      autoRefreshToken: true,
+      persistSession: false,
+      detectSessionInUrl: true
+    }
+  })
 }
